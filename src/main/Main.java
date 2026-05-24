d30a35a7017642643f7dd5ce1df40d9fb041dcac
Zin Wai Yan <zinwaiyan2226@gmail.com>
2026-05-25 02:27:24 +0900

    JDBC 데이터베이스 연결 설정

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,17 +1,20 @@
-package DB2026Team02;
+package DB2026Team02.main;
 
+import DB2026Team02.db.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 /**
  * 대학원 박람회 상담 예약 및 운영 관리 시스템 - 진입점
  */
+
 public class Main {
-
-    private static final String JDBC_URL =
-            "jdbc:mysql://localhost:3306/DB2026Team02?useSSL=false&serverTimezone=Asia/Seoul&characterEncoding=UTF-8";
-    private static final String DB_USER = "DB2026Team02";
-    private static final String DB_PASSWORD = "DB2026Team02";
-
     public static void main(String[] args) {
         System.out.println("DB2026Team02 - 대학원 박람회 상담 예약 시스템");
-        // TODO: 메뉴 및 JDBC 기능 구현
+        try (Connection conn = DatabaseConnection.getConnection()) {
+            System.out.println("DB 연결 성공!");
+        } catch (SQLException e) {
+            System.out.println("DB 연결 실패: " + e.getMessage());
+        }
     }
 }
